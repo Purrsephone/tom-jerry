d718d6a26568f803b904d2ee1904c205195bfd44
Purrsephone <askmeowtech@gmail.com>
2021-05-22 20:00:28 -0500

    god bless the broken code

--- state.py
+++ state.py
@@ -51,6 +51,12 @@ class Grid:
         #initialize states 
         self.states = []
 
+        #initialize actions
+        self.actions = []
+
+        #initialze action matrix 
+        self.action_matrix = []
+
         # subscribe to the map server
         rospy.Subscriber(self.map_topic, OccupancyGrid, self.get_map)
 
@@ -154,4 +160,95 @@ class Grid:
             #1 coordinates of midpoint of the square 
             #2 directions that the robot can move from that state 
             #3 current orientation of the robot 
+    def possible_transition(self, state1, state2):
+        #if both square and direction are different, then no 
+        diffsquare = True 
+        if ((state1[0][0] == state2[0][0]) and (state1[0][1] == state2[0][1])):
+            diffsquare = False 
+        diffdirection = True 
+        if (state1[2] == state2[2]):
+            diffdirection = False 
+        if(diffsquare and diffdirection):
+            return False 
+        #if both square and direction are the same, then yes 
+        if(not(diffsquare) and not(diffdirection)):
+            return True 
+        #if squares are different, are they adjacent? 
+        if(diffsquare):
+            diffx = abs(state1[0][0] - state2[0][0])
+            diffy = abs(state1[0][1] - state2[0][1])
+            #are adjacent 
+            if((diffx == self.square_side_len) or (diffy == self.square_side_len)):
+                return True 
+            else:
+                return False 
+        #if directions are different, are they adjacent? 
+        if(diffdirection):
+            #are adjacent 
+            if((state1[2] % 2) == 0):
+                if((state2[2] % 2) != 0):
+                    return True 
+            #are adjacent 
+            if((state1[2] % 2) != 0):
+                if((state2[2] % 2) == 0):
+                    return True 
+            else 
+                return False 
+
+    #actions are:
+    #0 = go forward 
+    #1 = 90 degree turn CW 
+    #2 = 90 degree turn CCW 
+    #3 = do nothing 
+    def necessary_action(self, state1, state2):
+        diffsquare = True 
+        if ((state1[0][0] == state2[0][0]) and (state1[0][1] == state2[0][1])):
+            diffsquare = False 
+        diffdirection = True 
+        if (state1[2] == state2[2]):
+            diffdirection = False 
+        if ((not diffsquare) and (not diffdirection)):
+            return 3 
+        if diffsquare:
+            return 0
+        else:
+            if(state2[2] > state1[2]):
+                if(state1[2] != 0):
+                    return 1
+                else:
+                    return 2 
+            else: 
+                if(state2[2 < state1[2]]):
+                    if(state1[2] != 3):
+                        return 2
+                    else:
+                        return 1 
+                 
+    #make 2D array (state1 vs. state2)
+    def make_action_matrix(self):
+        num_states = len(self.states)
+        self.action_matrix = np.zeros((num_states, num_states))
+        outer_loop_counter = 0 
+        for state in self.states:
+            inner_loop_counter = 0 
+            for state2 in self.states:
+                valid = possible_transition(state, state2)
+                if(valid):
+                    action_needed = necessary_action(state, state2)
+                    self.action_matrix[outer_loop_counter][inner_loop_counter] = action_needed
+                else: 
+                     self.action_matrix[outer_loop_counter][inner_loop_counter] = -1 
+                inner_loop_counter += 1
+            outer_loop_counter += 1 
+        self.action_matrix
+    
+    def make_action_list(self):
+        for x in range(4):
+            self.actions.append(x)
+
+    
+            
+            
+
+        
             
