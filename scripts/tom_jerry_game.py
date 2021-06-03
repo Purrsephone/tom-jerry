c7acae62365a9d13adb919c9e30e45fe9791b970
Your Name <you@example.com>
2021-06-03 16:47:41 -0500

    now saving state matrices as csv

--- scripts/tom_jerry_game.py
+++ scripts/tom_jerry_game.py
@@ -2,14 +2,14 @@
 import state
 import os
 import copy
-import pickle
-import time
+import csv
 
 class TJGame(object):
     """
     A class encapsulating the information in a tom&jerry game for the purposes
     of q_learning, i.e., states, actions, and the state action matrix;
     Acts as an interface between state.py and minimax_qlearning.py
+    Note that the cat is the maximizer and the mouse is the minimizer
     """
     def __init__(self, compute_game_info:bool, load_from_file:bool, game_info_prefix:str, mouse_on_cheese):
         self.path_prefix = os.path.dirname(__file__) + '/state_action_matrices/' + game_info_prefix
@@ -39,32 +39,55 @@ class TJGame(object):
 
 
     def save_info(self):
-        with open(self.path_prefix + 'states.pckl', 'wb') as state_file:
-            pickle.dump(self.states, state_file)
-
-        with open(self.path_prefix + 'max_actions.pckl', 'wb') as max_actions:
-            pickle.dump(self.maximizer_actions, max_actions)
-
-        with open(self.path_prefix + 'min_actions.pckl', 'wb') as min_actions:
-            pickle.dump(self.minimizer_actions, min_actions)
-
-        with open(self.path_prefix + 'state_action_matrix.pckl', 'wb') as sam:
-            pickle.dump(self.state_action_matrix, sam)
+        with open(self.path_prefix + 'states.csv', 'w') as state_csv:
+            csv_writer = csv.writer(state_csv)
+            for state in self.states:
+                csv_writer.writerow([state.catpos.x, state.catpos.y, state.catpos.z, state.mousepos.x, state.mousepos.y, state.mousepos.z])
+        with open(self.path_prefix + 'max_action.csv', 'w') as max_action_csv:
+            csv_writer = csv.writer(max_action_csv)
+            for action in self.maximizer_actions:
+                csv_writer.writerow([action])
+        with open(self.path_prefix + 'min_action.csv', 'w') as min_action_csv:
+            csv_writer = csv.writer(min_action_csv)
+            for action in self.minimizer_actions:
+                csv_writer.writerow([action])
+        with open(self.path_prefix + 'state_action_matrix.csv', 'w') as sam_csv:
+            csv_writer = csv.writer(sam_csv)
+            for state_row in self.state_action_matrix:
+                csv_row = []
+                for resulting_state_index in state_row:
+                    csv_row.append(resulting_state_index)
+                csv_writer.writerow([csv_row])
 
 
     def load_info(self):
-        with open(self.path_prefix + 'states.pckl', 'rb') as state_file:
-            self.states = pickle.load(state_file)
-
-        with open(self.path_prefix + 'max_actions.pckl', 'rb') as max_actions:
-            self.maximizer_actions = pickle.load(max_actions)
-
-        with open(self.path_prefix + 'max_actions.pckl', 'rb') as min_actions:
-            self.minimizer_actions = pickle.load(min_actions)
-
-        with open(self.path_prefix + 'state_action_matrix.pckl', 'rb') as sam:
-            self.state_action_matrix = pickle.load(sam)
-    
+        self.states = []
+        self.maximizer_actions = []
+        self.minimizer_actions = []
+        self.state_action_matrix = []
+        with open(self.path_prefix + 'states.csv', 'r') as state_csv:
+            csv_reader = csv.reader(state_csv)
+            for row in csv_reader:
+                row = [float(x) for x in row]
+                new_state = state.State(state.Position(row[0], row[1], row[2]), state.Position(row[3], row[4], row[5]))
+                self.states.append(new_state)
+                
+        with open(self.path_prefix + 'max_action.csv', 'r') as max_action_csv:
+            csv_reader = csv.reader(max_action_csv)
+            for row in csv_reader:
+                self.maximizer_actions.append(int(row[0]))
+        with open(self.path_prefix + 'min_action.csv', 'r') as min_action_csv:
+            csv_reader = csv.reader(min_action_csv)
+            for row in csv_reader:
+                self.minimizer_actions.append(int(row[0]))
+        with open(self.path_prefix + 'state_action_matrix.csv', 'r') as sam_csv:
+            csv_reader = csv.reader(sam_csv)
+            for row in csv_reader:
+                state_row = []
+                for resulting_state in row:
+                    state_row.append(int(resulting_state))
+                self.state_action_matrix.append(state_row)
+                
 
     def reward(self, state, cat_action, mouse_action ):
         next_state = self.state_action_matrix[state][cat_action*len(self.maximizer_actions) + mouse_action]
@@ -95,5 +118,5 @@ class TJGame(object):
 
 if __name__ == "__main__":
     game_info_calc = TJGame(compute_game_info=True, load_from_file=False, game_info_prefix="TJGame1", mouse_on_cheese={})
-    #game_info_calc = TJGame(compute_game_info=False, load_from_file=True, game_info_prefix="TJGame1", mouse_on_cheese={})
+    # game_info_calc = TJGame(compute_game_info=False, load_from_file=True, game_info_prefix="TJGame1", mouse_on_cheese={})
     game_info_calc.print_board_positions()
